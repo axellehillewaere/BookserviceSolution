add66c36dca3cf3d5fbc930a4aa746a23aabc2b4
Axelle Hillewaere <axelle.hillewaere@hotmail.be>
2018-10-18 09:52:53 +0200

    Add the Publisher model

---
+++ Bookservice.WebAPI/Models/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookservice.WebAPI.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+    }
+}
